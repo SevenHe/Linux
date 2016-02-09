1cca8ea7d92a2ba5143d4cd9bce48771d027c0ca
SEVEN <sevenhe2015@gmail.com>
2016-02-10 00:28:15 +0800

    studying Perl on the way..

---
+++ Perl/AfterSixTest.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+
+use warnings;
+
+sub popmany {
+	my @retlist = ();
+	for my $aref(@_) {
+		print "aref is '@$aref'!\n";
+		push @retlist, pop @$aref;
+	}
+
+	return @retlist;
+}
+
+@a = (1, 3, 5, 7);
+@b = (2, 4, 6, 8);
+@c = (5, 6, 7, 8);
+$d = \@c;
+$e = [1, 2];							# "[]" is to create a reference, and it is a entirety.
+@f = [6, 7, 8, 9];
+$g = @a;
+$h = [ @b ];
+
+@tailings = popmany(\@a, \@b, \@c);		# equivalent to "\(@a, @b, @c)"!
+
+for $i (0..$#a) {
+	print "row $i is $a[$i].\n";
+}
+
+print "tailings is '@tailings'!\n";
+print "OK!\n" if exists $d->[2]; 
+print "[ $e ], g:$g, [ @f ], $a[1], $f[0], @$h\n";
+
+%family = (
+	johns => ["barney"],
+	bestons => ["ok", "1fdafa3"],
+	simpsons => ["sdfj", "654ok", "third"]
+);
+$family{"seven"} = ["inotok", "jd", "asf", "sdfe2"];
+
+# sort {...}, the expression is the thing you want to compare, and the following is to compare the values 
+# instead of the keys. 
+for $member (sort { @{$family{$a}} <=> @{$family{$b}} } keys %family) {
+	print "$member : @{$family{$member}} \n";
+}
+
+#use 'func (\[$@%])' to ensure the argument is what you specify, on the contrary, the 'func ([$@%])' is just 
+#providing the program environment.
