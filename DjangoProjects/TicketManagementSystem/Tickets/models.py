69898917f914867734ecbec19cc484654a6381da
SEVEN <sevenhe2015@gmail.com>
2016-02-27 21:06:26 +0800

    update some files and templates..

--- DjangoProjects/TicketManagementSystem/Tickets/models.py
+++ DjangoProjects/TicketManagementSystem/Tickets/models.py
@@ -1,5 +1,27 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class TrainTicket(models.Model):
+    serial = models.CharField(max_length=4)
+    departure = models.DateTimeField()
+    last = models.IntegerField()
+    tp = models.CharField(max_length=1)
+    price = models.Model(max_digits=5, decimal_places=1)
+    quantity = models.IntegerField()
+
+
+class Trade(models.Model):
+    STATUS_CHOICES = (
+            (1, 'BOOKED'),
+            (2, 'BOUGHT'),
+            (3, 'DISABLED'),
+            )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    serial = models.ForeignKey(TrainTicket)
+    book_date = models.DateTimeField()
+    number = models.IntegerField(default=1)
+    status = models.IntegerField(default=1, choices=STATUS_CHOICES)
+
