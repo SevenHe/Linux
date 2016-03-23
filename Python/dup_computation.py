87dfdb23777560cda536721ef72663546f62d67f
SEVEN <sevenhe2015@gmail.com>
2016-03-23 19:45:28 +0800

    just a update..

---
+++ Python/dup_computation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import sys
+
+
+inputs = raw_input()
+count = 0
+temp_c = ''
+last = ''
+final = ''
+for char in inputs:
+    if 'a' <= char <= 'z':
+        char = char.upper()
+    if 'A' <= char <= 'Z':
+        if temp_c == '':
+            temp_c = char
+            count = 1
+        elif temp_c != '0':
+            if temp_c == char:
+                count += 1
+            else:
+                final += "%s%d" % (temp_c, count)
+                last = temp_c
+                temp_c = char
+                count = 1
+
+if last != '':
+    final += "%s%d" % (temp_c, count)
+
+if final != '':
+    print final
