48ba9be51b8a6a14dc6f26752e7784b1d8d20ea5
SEVEN <sevenhe2015@gmail.com>
2016-03-23 19:50:38 +0800

    just a update..

--- Python/dup_computation.py
+++ Python/dup_computation.py
@@ -7,21 +7,22 @@ count = 0
 temp_c = ''
 last = ''
 final = ''
-for char in inputs:
-    if 'a' <= char <= 'z':
-        char = char.upper()
-    if 'A' <= char <= 'Z':
-        if temp_c == '':
-            temp_c = char
-            count = 1
-        elif temp_c != '0':
-            if temp_c == char:
-                count += 1
-            else:
-                final += "%s%d" % (temp_c, count)
-                last = temp_c
+if len(inputs) < 1024:
+    for char in inputs:
+        if 'a' <= char <= 'z':
+            char = char.upper()
+        if 'A' <= char <= 'Z':
+            if temp_c == '':
                 temp_c = char
                 count = 1
+            elif temp_c != '0':
+                if temp_c == char:
+                    count += 1
+                else:
+                    final += "%s%d" % (temp_c, count)
+                    last = temp_c
+                    temp_c = char
+                    count = 1
 
 if last != '':
     final += "%s%d" % (temp_c, count)
